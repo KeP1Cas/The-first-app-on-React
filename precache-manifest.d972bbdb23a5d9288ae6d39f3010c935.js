3bc91fb0d51ad30fbadd2b38c99f0df4cb472e37
Danil <daniklucky38@gmail.com>
2020-08-14 01:06:49 +0400

    Updates

---
+++ precache-manifest.d972bbdb23a5d9288ae6d39f3010c935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01fe5046f3ca8f64bf5f99175343ff93",
+    "url": "/The-first-app-on-React/index.html"
+  },
+  {
+    "revision": "3d4cbdff5c491c78d679",
+    "url": "/The-first-app-on-React/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "4cb72cd24c4409f72079",
+    "url": "/The-first-app-on-React/static/css/main.556cc4d0.chunk.css"
+  },
+  {
+    "revision": "3d4cbdff5c491c78d679",
+    "url": "/The-first-app-on-React/static/js/2.cc15e400.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/The-first-app-on-React/static/js/2.cc15e400.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb72cd24c4409f72079",
+    "url": "/The-first-app-on-React/static/js/main.345c5a62.chunk.js"
+  },
+  {
+    "revision": "2704e9343af61defc147",
+    "url": "/The-first-app-on-React/static/js/runtime-main.c082de3a.js"
+  }
+]);
